d4e82f2a05e7bfb1b25ac4c37d6fe5ee6656e4fe
Zabih Keraam <165507052+Zabihkeraam1@users.noreply.github.com>
2025-04-05 13:29:11 +0430

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 const {app} = require('./app.js');
 const connectDB = require('./dbconnection.js');
-const port = 3001;
+const port = 8080;
 connectDB()
 app.listen(port, () => {
     console.log(`API server running on http://localhost:${port}`);
