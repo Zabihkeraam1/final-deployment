c514952d01f5134219186f2e0eb83079600fc5fc
Zabih Keraam <165507052+Zabihkeraam1@users.noreply.github.com>
2025-04-05 16:00:03 +0430

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -6,5 +6,8 @@ const router = require('./routers/index.js');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', router);
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
 
 module.exports = {app};
