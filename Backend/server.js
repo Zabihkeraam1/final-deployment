f30dfc4144f797c00fb46e2bfa1a9ef6b57db7a0
Zabih Keraam <165507052+Zabihkeraam1@users.noreply.github.com>
2025-04-05 16:20:11 +0430

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 const {app} = require('./app.js');
 const connectDB = require('./dbconnection.js');
 const PORT = process.env.PORT || 8080;
-connectDB()
+// connectDB()
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on ${PORT}`);
 });
