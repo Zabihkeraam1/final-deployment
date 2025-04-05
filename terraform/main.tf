6fcb1fc15b4907cded45b8850ca191100586a749
Zabih Keraam <165507052+Zabihkeraam1@users.noreply.github.com>
2025-04-05 15:44:16 +0430

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,7 +45,7 @@ resource "aws_apprunner_service" "backend_service" {
           configuration_source = "API"  # Use "API" if using apprunner.yaml
         code_configuration_values {
           runtime = "NODEJS_18"       # Node.js 18
-          build_command = "cd backed && npm install"
+          build_command = "cd backend && npm install"
           start_command = "cd backend && npm start"
           port = "8080"               # Your app's port
         }
