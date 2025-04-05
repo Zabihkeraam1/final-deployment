df2d5c69b36f91c2cb72bb1ede8856709334c206
Zabih Keraam <165507052+Zabihkeraam1@users.noreply.github.com>
2025-04-05 14:50:01 +0430

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,19 +23,13 @@ provider "aws" {
   region = "us-east-1" # Change to your region
 }
 
-# 1️⃣ GitHub Connection (fully managed by Terraform)
-resource "aws_apprunner_connection" "github_connection" {
-  connection_name = "github-monorepo-connection"
-  provider_type   = "GITHUB"
-}
-
-# 2️⃣ App Runner Service (backend folder in monorepo)
+# 1️⃣ App Runner Service (backend folder in monorepo)
 resource "aws_apprunner_service" "backend_service" {
   service_name = "monorepo-backend-service"
 
   source_configuration {
     authentication_configuration {
-      connection_arn = aws_apprunner_connection.github_connection.arn
+      connection_arn = "arn:aws:apprunner:us-east-1:135808921133:connection/github-monorepo-connection/d9696a6262774e5a8fdb36bd36dccf3f"
     }
 
     auto_deployments_enabled = true
