a8b30a9ee52db02921de1a58e9cf257004a36dfc
Zabih Keraam <165507052+Zabihkeraam1@users.noreply.github.com>
2025-04-08 17:01:13 +0430

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,20 @@ variable "AWS_SECRET_ACCESS_KEY" {
   type        = string
   sensitive   = true
 }
+
+variable "customer_name" {
+  type        = string
+  description = "Name of the customer"
+}
+
+variable "project" {
+  type        = string
+  description = "Name of the project"
+}
+
+
+variable "region" {
+  type        = string
+  default     = "us-east-1"
+  description = "AWS region"
+}
